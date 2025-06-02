9c63e2fe0100ee0b94158a43324a2ad97718405c
Ivan Sigaev <ivan.develop@gmail.com>
2025-06-02 21:59:24 +0200

    feat: add valid UUID

--- src/album/dto/response.dto.ts
+++ src/album/dto/response.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class AlbumResponseDto {
   @ApiProperty({
-    example: '11111111-ab12-ab32-1234-123123123123',
+    example: '0a35dd62-e09f-444b-a628-f4e7c6954f57',
     description: 'UUID',
   })
   id: string;
@@ -20,7 +20,7 @@ export class AlbumResponseDto {
   year: number;
 
   @ApiProperty({
-    example: '11111111-ab12-ab32-1234-123123123123',
+    example: '0a35dd62-e09f-444b-a628-f4e7c6954f57',
     description: 'UUID of the artist',
   })
   artistId: string;
--- src/user/dto/response.dto.ts
+++ src/user/dto/response.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class UserResponseDto {
   @ApiProperty({
-    example: '11111111-ab12-ab32-1234-123123123123',
+    example: '0a35dd62-e09f-444b-a628-f4e7c6954f57',
     description: 'UUID',
   })
   id: string;
